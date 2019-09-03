7bf21c8f4d712e1f7f26cff42e83436706df0a42
quyongchao <1409062321@qq.com>
2019-09-03 11:38:11 +0800

    主角

---
+++ black-cat/Assets/Sprites/quyongchao/A Protagonist/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    float blastspeed;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        blastspeed = speed * 2.0f;
+        Vector2 sudu = new Vector2(speed, 0);
+        GetComponent<Rigidbody2D>().velocity = sudu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
