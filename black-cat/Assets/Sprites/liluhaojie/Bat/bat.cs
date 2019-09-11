66376c9498a9dba379fcf65d745661d43e63ca19
sdjawhd <52514389+sdjawhd@users.noreply.github.com>
2019-09-11 14:23:04 +0800

    补

---
+++ black-cat/Assets/Sprites/liluhaojie/Bat/bat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bat : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public Transform target;//目标位置
+  public float distance;//两个物体的距离
+  void Start () {
+    distance = Vector2.Distance (transform.position,target.position);
+	
+    }
+       void Update () {
+       transform.position = Vector2.MoveTowards (transform.position,target.position,(distance/200f)*Time.deltaTime);
+  }
+
+
+}
---
+++ black-cat/Assets/Sprites/liluhaojie/Bat/redbat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class redbat : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public float speed = 5.0f;
+	float blastspeed;
+    void Start()
+    {
+        blastspeed = speed * 2.0f;
+		Vector2 sudu = new Vector2 (-speed,0);
+		GetComponent<Rigidbody2D>().velocity = sudu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
