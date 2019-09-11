a1fa3265f8aea5ef8ccdc225b497b55238857957
sdjawhd <52514389+sdjawhd@users.noreply.github.com>
2019-09-11 14:36:03 +0800

    补

---
+++ black-cat/Assets/Sprites/liluhaojie/Water and fire drops/water_drop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water_drop : MonoBehaviour
+{
+    public GameObject water;
+	public float Timestart= 0;
+	public float TimeInterval = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timestart += Time.deltaTime;
+		if(Timestart>=TimeInterval){
+			if(water){
+			Vector2 newwater = new Vector2(transform.position.x-0.07f,transform.position.y-0.25f);
+			Instantiate(water,newwater,Quaternion.identity);
+			}
+			Timestart = 0;
+		}
+    }
+}
